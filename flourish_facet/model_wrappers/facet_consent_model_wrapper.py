b392a6ec9fc76daedd20e588b07c07f191a4e57a
Moses Chawawa <moseschawawa@gmail.com>
2023-08-28 14:07:43 +0200

    Listboard and validations need to be revisited

--- flourish_facet/model_wrappers/facet_consent_model_wrapper.py
+++ flourish_facet/model_wrappers/facet_consent_model_wrapper.py
@@ -3,7 +3,7 @@ from django.apps import apps as django_apps
 from django.conf import settings
 from edc_model_wrapper import ModelWrapper
 
-class FacetConsentModelWrapper():
+class FacetConsentModelWrapper(ModelWrapper):
     model = 'flourish_facet.facetconsent'
-    # next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_listboard_url')
+    next_url_name = settings.DASHBOARD_URL_NAMES.get('flourish_facet_consent_listboard_url')
     next_url_attrs = []
