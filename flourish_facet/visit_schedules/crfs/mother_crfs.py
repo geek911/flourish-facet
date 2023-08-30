f83b1ccd521b6674c1abed10864b06ce35c8b1b2
Moses Chawawa <moseschawawa@gmail.com>
2023-08-30 11:47:28 +0200

    Added mother crfs

--- flourish_facet/visit_schedules/crfs/mother_crfs.py
+++ flourish_facet/visit_schedules/crfs/mother_crfs.py
@@ -1,3 +1,7 @@
 from edc_visit_schedule import FormsCollection, Crf
 
-mother_crfs = FormsCollection(name='facet_enrollment')
+mother_crfs = FormsCollection(
+    Crf(show_order=1, model='flourish_facet.householdhungerscale'),
+    Crf(show_order=2, model='flourish_facet.intimatepartnerviolence'),
+    Crf(show_order=3, model='flourish_facet.maternalhivart'),
+    name='facet_enrollment')
