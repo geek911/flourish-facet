3be27ffa376b752e978101333211e58e66e1d4b3
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-18 15:10:31 +0200

    Refactor validations

--- flourish_facet/forms/mother/maternal_hiv_art_form.py
+++ flourish_facet/forms/mother/maternal_hiv_art_form.py
@@ -1,9 +1,11 @@
 from django import forms
 from ...models import MaternalHivArt
 from edc_base.sites import SiteModelFormMixin
+from ...form_validators import MaternalHivArtFormValidator
 
 
 class MaternalHivArtForm(SiteModelFormMixin, forms.ModelForm):
+    form_validator_cls = MaternalHivArtFormValidator
 
     class Meta:
         model = MaternalHivArt
