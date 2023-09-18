8a24513e041481e9a83e0a6c6218cf9b101835a5
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-18 09:24:09 +0200

    Add more validations to Child_hiv_testing

--- flourish_facet/form_validators/child_hiv_testing_validation.py
+++ flourish_facet/form_validators/child_hiv_testing_validation.py
@@ -1,5 +1,5 @@
 from edc_form_validators import FormValidator
-from edc_constants.constants import NO
+from edc_constants.constants import NO, YES
 
 
 class ChildHivTestingFormValidator(FormValidator):
@@ -10,6 +10,15 @@ class ChildHivTestingFormValidator(FormValidator):
 
         self.required_if(NO, field='child_tested_6_weeks',
                          field_required='reason_not_tested_6_weeks')
+
+        self.required_if(YES, field='child_breastfed',
+                         field_required='child_breastfed_end')
+
+        self.not_applicable_if(YES, field='child_tested',
+                               field_applicable='reason_not_tested')
+
+        self.not_applicable_if(
+            YES, field='child_tested_6_weeks', field_applicable='reason_not_tested_6_weeks')
 
         self.validate_other_specify(
             field='reason_not_tested', other_specify_field='reason_not_tested_other')
