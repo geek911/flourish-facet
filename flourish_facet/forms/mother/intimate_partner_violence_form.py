a86f261c0b483efaec767a30c7282c4046eba03b
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-19 09:18:40 +0200

    Refactor form

--- flourish_facet/forms/mother/intimate_partner_violence_form.py
+++ flourish_facet/forms/mother/intimate_partner_violence_form.py
@@ -1,9 +1,10 @@
 from django import forms
 from ...models import IntimatePartnerViolence
 from edc_form_validators import FormValidatorMixin
+from edc_base.sites import SiteModelFormMixin
 
 
-class IntimatePartnerViolenceForm(FormValidatorMixin, forms.ModelForm):
+class IntimatePartnerViolenceForm(FormValidatorMixin, SiteModelFormMixin, forms.ModelForm):
 
     class Meta:
         model = IntimatePartnerViolence
