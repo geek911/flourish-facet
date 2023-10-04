d0706872d9733146fe200cd71bf7eb47733e00c8
Moses Chawawa <moseschawawa@gmail.com>
2023-10-04 11:51:51 +0200

    Added formulas for calculating depressions scores

--- flourish_facet/models/mother/anxiety_screening_gad_7.py
+++ flourish_facet/models/mother/anxiety_screening_gad_7.py
@@ -3,6 +3,21 @@ from ..model_mixins import CrfModelMixin
 
 
 class AnxietyScreeningGad7(CaregiverGadAnxietyScreeningMixin, CrfModelMixin):
+
+    def save(self, *args, **kwargs):
+        self.anxiety_score = self.calculate_depression_score
+        super().save(*args, **kwargs)
+
+    @property
+    def calculate_depression_score(self):
+        score = 0
+        for f in self._meta.get_fields():
+            if f.name in ['feeling_anxious', 'control_worrying', 'worrying',
+                          'trouble_relaxing', 'restlessness', 'easily_annoyed',
+                          'fearful', ]:
+                score += int(getattr(self, f.name))
+        return score
+
     class Meta(CrfModelMixin.Meta):
         app_label = 'flourish_facet'
         verbose_name = "Anxiety Screening GAD-7"
