a54f9b7449b399082d68800e7e07f722b7a15fb5
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-10-10 15:43:19 +0200

    Refactor initimate partner violence validation

--- flourish_facet/form_validators/intimate_partner_violence_form_validation.py
+++ flourish_facet/form_validators/intimate_partner_violence_form_validation.py
@@ -1,7 +1,5 @@
 from edc_form_validators import FormValidator
-from edc_constants.constants import NO, YES
 from edc_constants.choices import NOT_APPLICABLE
-from django.core.exceptions import ValidationError
 
 
 class IntimatePartnerViolenceFormValidator(FormValidator):
@@ -11,14 +9,14 @@ class IntimatePartnerViolenceFormValidator(FormValidator):
         self.validate_violence(cleaned_data=self.cleaned_data)
 
     def validate_violence(self, cleaned_data=None):
-
+        responses = ['never', NOT_APPLICABLE]
         physically_hurt = cleaned_data.get('physically_hurt')
         insult_talk = cleaned_data.get('insult_talk')
         threaten = cleaned_data.get('threaten')
         scream_curse = cleaned_data.get('scream_curse')
+        not_applicable = all([physically_hurt in responses, insult_talk in responses,
+                             threaten in responses, scream_curse in responses])
 
         self.applicable_if_true(
-            physically_hurt in ['rarely', 'sometimes', 'fairly_often', 'frequently'
-                                ] or insult_talk in ['rarely', 'sometimes', 'fairly_often', 'frequently'
-                                                     ] or threaten in ['rarely', 'sometimes', 'fairly_often', 'frequently'] or scream_curse in ['rarely', 'sometimes', 'fairly_often', 'frequently'],
+            not not_applicable,
             field_applicable='referral')
