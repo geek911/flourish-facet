e6068552b4b543dd36dd14d54b3c8ba102621024
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-10-12 14:45:38 +0200

    Refactor the qualitative models forms and validations

--- flourish_facet/form_validators/qualitative_interview_audio_uploads_validation.py
+++ flourish_facet/form_validators/qualitative_interview_audio_uploads_validation.py
@@ -1,6 +1,4 @@
 from edc_form_validators import FormValidator
-from edc_constants.constants import YES, NO
-from django.core.exceptions import ValidationError
 
 
 class QualitativeInterviewAudioUploadsFormValidator(FormValidator):
--- flourish_facet/forms/mother/qualitative_interview_translation_transcription_form.py
+++ flourish_facet/forms/mother/qualitative_interview_translation_transcription_form.py
@@ -1,9 +1,10 @@
 from django import forms
 from ...models import QualitativeInterviewTranscriptionAndTranslation
 from edc_base.sites import SiteModelFormMixin
+from edc_form_validators import FormValidatorMixin
 
 
-class QualitativeInterviewTranscriptionAndTranslationForm(SiteModelFormMixin, forms.ModelForm):
+class QualitativeInterviewTranscriptionAndTranslationForm(FormValidatorMixin, SiteModelFormMixin, forms.ModelForm):
 
     class Meta:
         model = QualitativeInterviewTranscriptionAndTranslation
