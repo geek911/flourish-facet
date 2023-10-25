dec5ded474cf2e5f0c1a8ee423ff34a358c01502
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-10-25 12:04:30 +0200

    add facet home page

--- flourish_facet/views/__init__.py
+++ flourish_facet/views/__init__.py
@@ -4,3 +4,4 @@ from .mother import FlourishConsentListboardView
 from .child import FacetChildConsentListboardView
 from .child import FacetChildDashboardView
 from .administration_view import AdministrationView
+from .home_view import HomeView
