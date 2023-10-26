8f27925423c1c3f1c387ba7b316ac88c8d90be72
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-10-26 16:22:46 +0200

    Focus group interview model,form ,admin and listboard

---
+++ flourish_facet/admin/mother/focus_group_interview_audio_uploads_admin.py
@@ -0,0 +1,72 @@
+from typing import Any
+from django.contrib import admin
+from django.db.models.fields.related import ManyToManyField, RelatedField
+from django.db.models.query import QuerySet
+from django.forms.models import ModelMultipleChoiceField
+from django.http.request import HttpRequest
+from flourish_facet.models.list_models import FgfSubjectIdentifiers
+from flourish_facet.models.mother.facet_consent import FacetConsent
+from ...models import FocusGroupInterviewAudioUploads
+from ...admin_site import flourish_facet_admin
+from ...forms import FocusGroupInterviewAudioUploadsForm
+from edc_model_admin import audit_fieldset_tuple
+from ..modeladmin_mixins import ModelAdminMixin
+from edc_list_data import PreloadData
+from django.db.models import Q
+
+
+@admin.register(FocusGroupInterviewAudioUploads, site=flourish_facet_admin)
+class FocusGroupInterviewAudioUploadsAdmin(ModelAdminMixin, admin.ModelAdmin):
+    readonly_fields = ('group_identifier',)
+    form = FocusGroupInterviewAudioUploadsForm
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'group_identifier',
+                'paticipant_ids',
+                'report_datetime',
+                'location',
+                'location_other',
+                'interview_duration',
+                'interview_language',
+                'audio_file',
+                'notes',
+            ]}
+         ), audit_fieldset_tuple)
+
+    radio_fields = {
+        'location': admin.VERTICAL,
+        'interview_language': admin.VERTICAL,
+    }
+
+    filter_horizontal = ['paticipant_ids',]
+
+    def add_view(self, request, form_url='', extra_context=None):
+        self.load_list_data()
+        return super().add_view(request, form_url, extra_context)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'paticipant_ids':
+            kwargs['queryset'] = self.get_subject_identifiers
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+    @property
+    def get_subject_identifiers(self):
+        subject_identifiers = FacetConsent.objects.exclude(
+            subject_identifier__in=self.get_used_identifiers).values_list(
+            'subject_identifier', flat=True)
+        return subject_identifiers
+
+    @property
+    def get_used_identifiers(self):
+        return FocusGroupInterviewAudioUploads.objects.values_list(
+            'paticipant_ids__name', flat=True)
+
+    def load_list_data(self):
+        subject_identifiers = list(set(self.get_subject_identifiers))
+        subject_id_list = [(identifier, identifier)
+                           for identifier in subject_identifiers]
+
+        list_data = {'flourish_facet.fgfsubjectidentifiers': subject_id_list}
+        PreloadData(list_data=list_data)
---
+++ flourish_facet/admin/mother/focus_group_interview_transcription_translation_admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from ...models import FocusGroupInterviewTranscriptionAndTranslation
+from ...admin_site import flourish_facet_admin
+from ...forms import FocusGroupInterviewTranscriptionAndTranslationForm
+from edc_model_admin import audit_fieldset_tuple
+from ..modeladmin_mixins import ModelAdminMixin
+
+
+@admin.register(FocusGroupInterviewTranscriptionAndTranslation, site=flourish_facet_admin)
+class FocusGroupInterviewTranscriptionAndTranslationAdmin(ModelAdminMixin, admin.ModelAdmin):
+    form = FocusGroupInterviewTranscriptionAndTranslationForm
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'group_identifier',
+                'report_datetime',
+                'transcription_date',
+                'transcription_upload',
+                'facet_member_transcription',
+                'transcription_duration',
+            ]}
+         ), audit_fieldset_tuple)
+
+    radio_fields = {
+        'facet_member_transcription': admin.VERTICAL,
+        'transcription_duration': admin.VERTICAL,
+    }
