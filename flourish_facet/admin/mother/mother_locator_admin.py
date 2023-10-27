05f8e33211fe937500e4f9cd6813b77030abfa2a
Moses Chawawa <moseschawawa@gmail.com>
2023-10-27 08:29:28 +0200

    Added mother locator model

---
+++ flourish_facet/admin/mother/mother_locator_admin.py
@@ -0,0 +1,97 @@
+from django.contrib import admin
+from django.http import HttpResponseRedirect
+from edc_fieldsets import Fieldsets
+from edc_model_admin import audit_fieldset_tuple
+
+from ..modeladmin_mixins import ModelAdminMixin
+from ...admin_site import flourish_facet_admin
+from ...forms import MotherLocatorForm
+from ...models import MotherLocator, FacetConsent
+
+
+@admin.register(MotherLocator, site=flourish_facet_admin)
+class MotherLocatorAdmin(ModelAdminMixin, admin.ModelAdmin):
+    form = MotherLocatorForm
+
+    def response_add(self, request, obj, **kwargs):
+        response = self._redirector(obj)
+        return response if response else super(MotherLocatorAdmin, self).response_add(request, obj)
+
+    def response_change(self, request, obj):
+        response = self._redirector(obj)
+        return response if response else super(MotherLocatorAdmin, self).response_change(request, obj)
+
+    def _redirector(self, obj):
+        caregiver_locator = FacetConsent.objects.filter(
+            subject_identifier=obj.subject_identifier)
+        if caregiver_locator:
+            return HttpResponseRedirect(f'/subject/subject_dashboard/{obj.subject_identifier}/')
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'subject_identifier',
+                'screening_identifier',
+                'study_maternal_identifier',
+                'report_datetime',
+                'locator_date',
+                'first_name',
+                'last_name',
+                'mail_address',
+                'health_care_infant',
+                'may_visit_home',
+                'physical_address',
+                'may_call',
+                'subject_cell',
+                'subject_cell_alt',
+                'subject_phone',
+                'subject_phone_alt',
+                'may_call_work',
+                'subject_work_place',
+                'subject_work_phone',
+                'may_contact_indirectly',
+                'indirect_contact_name',
+                'indirect_contact_relation',
+                'indirect_contact_physical_address',
+                'indirect_contact_cell',
+                'indirect_contact_phone',
+                'has_caretaker',
+                'caretaker_name',
+                'caretaker_cell',
+                'caretaker_tel'
+            ]}
+         ), audit_fieldset_tuple)
+
+    radio_fields = {
+        'may_call': admin.VERTICAL,
+        'may_call_work': admin.VERTICAL,
+        'may_visit_home': admin.VERTICAL,
+        'may_contact_indirectly': admin.VERTICAL,
+        'has_caretaker': admin.VERTICAL,
+        'is_locator_updated': admin.VERTICAL,
+    }
+
+    search_fields = ['subject_identifier', 'study_maternal_identifier']
+
+    list_display = ('study_maternal_identifier', 'subject_identifier', 'may_visit_home',
+                    'may_call', 'may_call_work')
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj=obj)
+        subject_identifier = getattr(obj, 'subject_identifier', '')
+        if subject_identifier and 'P' in subject_identifier:
+            fieldsets = Fieldsets(fieldsets=fieldsets)
+            try:
+                fieldsets.insert_fields(
+                    *('is_locator_updated',), insert_after='caretaker_tel')
+            except AttributeError:
+                pass
+            else:
+                return fieldsets.fieldsets
+        return fieldsets
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj=obj, **kwargs)
+        if 'pre_flourish' in request.GET.get('next', None):
+            form.project_name = 'pre_flourish'
+        return form
---
+++ flourish_facet/forms/mother/mother_locator_form.py
@@ -0,0 +1,107 @@
+from itertools import chain
+
+from django import forms
+from django.db.models import ManyToManyField
+from edc_base.sites import SiteModelFormMixin
+from edc_constants.constants import YES
+from edc_form_validators import FormValidatorMixin
+
+from flourish_form_validations.form_validators import CaregiverLocatorFormValidator
+from ...models import MotherLocator
+from ...models import FacetConsent
+
+
+class MotherLocatorForm(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
+    form_validator_cls = CaregiverLocatorFormValidator
+
+    screening_identifier = forms.CharField(
+        label='Eligibility Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    subject_identifier = forms.CharField(
+        label='Subject Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+        required=False)
+
+    study_maternal_identifier = forms.CharField(
+        label='Study Caregiver Subject Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+        required=False)
+
+    first_name = forms.CharField(
+        label='First Name',
+        required=False)
+
+    last_name = forms.CharField(
+        label='Last Name',
+        required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            subject_consented = FacetConsent.objects.filter(
+                subject_identifier=self.initial.get('subject_identifier', None)).latest(
+                'report_datetime')
+        except FacetConsent.DoesNotExist:
+            pass
+        else:
+            self.fields['first_name'].widget = forms.TextInput(
+                attrs={'readonly': 'readonly'})
+            self.fields['last_name'].widget = forms.TextInput(
+                attrs={'readonly': 'readonly'})
+            self.initial['first_name'] = subject_consented.first_name
+            self.initial['last_name'] = subject_consented.last_name
+
+        # Allow form to update without screening identifier, updated before calling.
+        project_name = getattr(self, 'project_name', None)
+        if project_name == 'pre_flourish':
+            self.fields['screening_identifier'].required = False
+
+    def compare_instance_fields(self, prev_instance=None):
+        exclude_fields = ['modified', 'created', 'user_created', 'user_modified',
+                          'hostname_created', 'hostname_modified', 'device_created',
+                          'device_modified', 'report_datetime', 'subject_identifier',
+                          'is_locator_updated', 'action_identifier',
+                          'tracking_identifier', 'related_tracking_identifier',
+                          'parent_tracking_identifier', 'study_maternal_identifier',
+                          'locator_date', 'may_sms'
+                          ]
+        if prev_instance:
+            other_values = self.model_to_dict(prev_instance, exclude=exclude_fields)
+            values = {key: self.data.get(key) or None for key in other_values.keys()}
+            return values != other_values
+        return False
+
+    def model_to_dict(self, instance, exclude):
+        opts = instance._meta
+        data = {}
+        for f in chain(opts.concrete_fields, opts.private_fields, opts.many_to_many):
+            if not getattr(f, 'editable', False):
+                continue
+            if exclude and f.name in exclude:
+                continue
+            if isinstance(f, ManyToManyField):
+                data[f.name] = [str(obj.id) for obj in f.value_from_object(instance)]
+                continue
+            data[f.name] = f.value_from_object(instance) or None
+        return data
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        subject_identifier = self.cleaned_data.get('subject_identifier', None)
+        if subject_identifier:
+            try:
+                prev_instance = MotherLocator.objects.get(
+                    subject_identifier=subject_identifier)
+            except MotherLocator.DoesNotExist:
+                pass
+            else:
+                has_changed = self.compare_instance_fields(prev_instance)
+                if not has_changed and self.cleaned_data.get('is_locator_updated') == YES:
+                    raise forms.ValidationError(
+                        'No changes detected. Please update at least one field.')
+        return self.cleaned_data
+
+    class Meta:
+        model = MotherLocator
+        fields = '__all__'
