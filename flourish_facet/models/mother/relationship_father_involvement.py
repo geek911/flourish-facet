47fb89a744993f6c28e5b5a27227e55840175b51
Ame Diphoko <diphokoame@gmail.com>
2024-01-23 13:11:01 +0200

    update: m2m variables for relationship father involvement

--- flourish_facet/models/mother/relationship_father_involvement.py
+++ flourish_facet/models/mother/relationship_father_involvement.py
@@ -1,5 +1,6 @@
 from django.db import models
-from flourish_caregiver.models.list_models import HouseholdMember
+from flourish_caregiver.models.list_models import (MemberReadBooks, MemberChildOutside, MemberNamedWithChild,
+                                                   MemberPlayedWithChild, MemberSangSongs, MemberToldStories)
 from flourish_caregiver.models.model_mixins import RelationshipFatherInvolvementMixin
 from flourish_facet.models.model_mixins.crf_model_mixin import CrfModelMixin
 
@@ -9,32 +10,32 @@ class FacetRelationshipFatherInvolvement(RelationshipFatherInvolvementMixin, Crf
     at the 6 month visit
     """
     read_books = models.ManyToManyField(
-        HouseholdMember,
+        MemberReadBooks,
         related_name='facet_read_books',
         verbose_name='Read books or looked at picture books with your child', )
 
     told_stories = models.ManyToManyField(
-        HouseholdMember,
+        MemberToldStories,
         related_name='facet_told_stories',
         verbose_name='Told stories to your child', )
 
     sang_songs = models.ManyToManyField(
-        HouseholdMember,
+        MemberSangSongs,
         related_name='facet_sang_songs',
         verbose_name='Sang songs to or with your child, including lullabies', )
 
     took_child_outside = models.ManyToManyField(
-        HouseholdMember,
+        MemberChildOutside,
         related_name='facet_took_child_outside',
         verbose_name='Took your child outside the home', )
 
     played_with_child = models.ManyToManyField(
-        HouseholdMember,
+        MemberPlayedWithChild,
         related_name='facet_played_with_child',
         verbose_name='Played with your child', )
 
     named_with_child = models.ManyToManyField(
-        HouseholdMember,
+        MemberNamedWithChild,
         related_name='facet_named_with_child',
         verbose_name='Named, counted, or drew things with or for your child', )
 
