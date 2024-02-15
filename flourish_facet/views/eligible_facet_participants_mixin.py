8a435afdddf817ee6cfa18b1ff2af83e6ff061b4
Moses Chawawa <moseschawawa@gmail.com>
2024-02-15 10:02:01 +0200

    removed caregiver who are offstudy

--- flourish_facet/views/eligible_facet_participants_mixin.py
+++ flourish_facet/views/eligible_facet_participants_mixin.py
@@ -2,7 +2,7 @@ from django.apps import apps as django_apps
 from edc_base.utils import get_utcnow
 from dateutil.relativedelta import relativedelta
 from edc_constants.constants import YES
-from django.db.models import Max
+from django.db.models import Max, Q
 
 
 class EligibleFacetParticipantsMixin:
@@ -11,6 +11,17 @@ class EligibleFacetParticipantsMixin:
     facet_screening_model = 'flourish_facet.facetsubjectscreening'
     facet_consent_model = 'flourish_facet.facetconsent'
     subject_consent_model = 'flourish_caregiver.subjectconsent'
+    caregiver_offstudy_model = 'flourish_prn.caregiveroffstudy'
+    child_offstudy_model = 'flourish_prn.childoffstudy'
+    flourish_child_consent_model = 'flourish_caregiver.caregiverchildconsent'
+
+    @property
+    def caregiver_offstudy_cls(self):
+        return django_apps.get_model(self.caregiver_offstudy_model)
+
+    @property
+    def child_offstudy_cls(self):
+        return django_apps.get_model(self.child_offstudy_model)
 
     @property
     def subject_consent_cls(self):
@@ -40,15 +51,21 @@ class EligibleFacetParticipantsMixin:
         dates_before = (get_utcnow() - relativedelta(months=6, days=10)
                         ).date().isoformat()
 
+        caregiver_offstudy_subject_identifiers = self.caregiver_offstudy_cls.objects.values_list(
+            'subject_identifier', flat=True)
+
+        child_offstudy_subject_identifiers = self.child_offstudy_cls.objects.values_list(
+            'subject_identifier', flat=True)
         today = get_utcnow().date().isoformat()
 
         anc_subject_identifiers = self.antenatal_enrollment_cls.objects. \
             values_list('subject_identifier', flat=True)
 
         subject_identifiers = self.flourish_child_consent_cls.objects.filter(
+            ~Q(subject_identifier__in=child_offstudy_subject_identifiers),
             child_dob__range=[dates_before, today],
             subject_consent__subject_identifier__in=anc_subject_identifiers,
-            subject_consent__future_contact=YES
+            subject_consent__future_contact=YES,
         ).values_list('subject_consent__subject_identifier', flat=True)
 
         facet_screened_identifiers = self.facet_screening_cls.objects.values_list(
@@ -60,12 +77,19 @@ class EligibleFacetParticipantsMixin:
         consent_ids = []
 
         for subject_identifier in subject_identifiers:
-            consent = self.subject_consent_cls.objects.filter(
-                subject_identifier=subject_identifier
-            ).latest('version')
 
-            consent_ids.append(consent.id)
+            try:
+                consent = self.subject_consent_cls.objects.filter(
+                    ~Q(subject_identifier__in=caregiver_offstudy_subject_identifiers),
+                    subject_identifier=subject_identifier
+                ).latest('version')
+            except self.subject_consent_cls.DoesNotExist:
+                pass
+            else:
+                consent_ids.append(consent.id)
 
-        return queryset.filter(id__in=consent_ids,
-                               subject_identifier__startswith='B').annotate(
+        return queryset.filter(
+
+            id__in=consent_ids,
+            subject_identifier__startswith='B',).annotate(
             child_dob=Max('caregiverchildconsent__child_dob'), ).order_by('child_dob')
