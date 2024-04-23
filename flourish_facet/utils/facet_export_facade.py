3dcb45652da69a9fb161b30247fa1ccd639aa805
Moses Chawawa <moseschawawa@gmail.com>
2024-04-23 10:52:48 +0200

    change: exports changed from excel to csv

--- flourish_facet/utils/facet_export_facade.py
+++ flourish_facet/utils/facet_export_facade.py
@@ -3,6 +3,7 @@ from flourish_export.views.export_methods_view_mixin import ExportMethodsViewMix
 from flourish_export.admin_export_helper import AdminExportHelper
 from flourish_export.identifiers import ExportIdentifier
 
+
 class FacetExportFacade(ExportMethodsViewMixin):
 
     def __init__(self, request: HttpRequest):
@@ -12,10 +13,8 @@ class FacetExportFacade(ExportMethodsViewMixin):
         self.export_identifier_cls = ExportIdentifier
         self.request = request
 
-
     def add_email(self, email):
         self.emails.append(email)
-
 
     def generate_export(self):
         super().generate_export(self.app_label, self.emails)
