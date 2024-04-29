4d1580086d8d50820c37552ceecb467ea215c585
Moses Chawawa <moseschawawa@gmail.com>
2024-04-29 16:52:21 +0200

    fix: bulk export csv fix

--- flourish_facet/admin/exportaction_mixin.py
+++ flourish_facet/admin/exportaction_mixin.py
@@ -121,7 +121,7 @@ class ExportActionMixin(AdminExportHelper):
         consent = self.consent_obj(subject_identifier=subject_identifier)
 
         if consent:
-            return consent.screening_identifier
+            return getattr(consent, 'screening_identifier', None)
         return None
 
     def consent_obj(self, subject_identifier: str):
